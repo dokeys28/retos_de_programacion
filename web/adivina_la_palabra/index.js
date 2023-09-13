97be99d434fb68e66cd83757740e9d6e6b17c8c3
dokeys <dokeys28@gmail.com>
2023-09-13 19:16:07 -0400

    initial

---
+++ web/adivina_la_palabra/index.js
@@ -0,0 +1,38 @@
+const LISTADEPALABRAS = ['penecito', 'agapito'];
+const OCU = ['p_ne_ito', 'a_a_it_'];
+const INICIAR = document.querySelector('.iniciar');
+const BOTON = document.querySelector('.enviar');
+const PALABRA = document.querySelector('.palabra');
+const USUARIO = document.querySelector('.input');
+
+let palabra_oculta;
+let palabra_random;
+
+function inicio(){
+    let numero_random = Math.round(Math.random());
+    palabra_random = LISTADEPALABRAS[numero_random];
+    palabra_oculta = OCU[numero_random];
+    PALABRA.innerHTML = palabra_oculta;
+    INICIAR.disabled = true
+
+};
+
+function verificar(){
+    if (USUARIO.value == palabra_random){
+        PALABRA.innerHTML = palabra_random;
+        INICIAR.disabled = false
+    }else{
+        PALABRA.innerHTML = 'klk mamaguevo';
+        INICIAR.disabled = false
+    };
+};
+
+
+INICIAR.addEventListener('click',()=>{
+    inicio();
+
+});
+BOTON.addEventListener('click',()=>{
+
+    verificar();
+});
