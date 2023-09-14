c582b124afb8ad26db270a5cfe6a3c36a7415ecf
dokeys <dokeys28@gmail.com>
2023-09-14 17:20:46 -0400

    volvi

---
+++ index.js
@@ -0,0 +1,43 @@
+const LISTADEPALABRAS = ['penecito', 'agapito'];
+const OCU = ['p_ne_ito', 'a_a_it_'];
+const INICIAR = document.querySelector('.iniciar');
+const BOTON = document.querySelector('.enviar');
+const PALABRA = document.querySelector('.palabra');
+const USUARIO = document.querySelector('.input');
+
+let palabra_oculta;
+let palabra_random;
+BOTON.disabled = true;
+
+function inicio(){
+    let numero_random = Math.round(Math.random());
+    palabra_random = LISTADEPALABRAS[numero_random];
+    palabra_oculta = OCU[numero_random];
+    PALABRA.innerHTML = palabra_oculta;
+    INICIAR.disabled = true;
+    USUARIO.value = "";
+    BOTON.disabled = false;
+};
+
+function verificar(){
+    if (USUARIO.value == palabra_random){
+        PALABRA.innerHTML = palabra_random;
+        INICIAR.disabled = false;
+        BOTON.disabled = true;
+        
+    }else{
+        PALABRA.innerHTML = 'klk mamaguevo';
+        INICIAR.disabled = false;
+        BOTON.disabled = true;
+    };
+};
+
+
+INICIAR.addEventListener('click',()=>{
+    inicio();
+
+});
+BOTON.addEventListener('click',()=>{
+
+    verificar();
+});
